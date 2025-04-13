2df1641c26c54eba6dd646fe78ffc0d7e8fdd078
Brenno <brenno@giomtecnologia.com.br>
2025-04-13 03:48:27 -0300

    Alteracao de movimentacao do jogador

--- Objects/Player.cs
+++ Objects/Player.cs
@@ -11,12 +11,13 @@ public class Player : GameObject
 
     public int Velocidade { get; set; }
 
+    //INTERNOS
+    public bool CanMove { get; set; }
+
     public Player()
     {
         Transform = new Transform();
         Sprite = new Sprite();
-
-        Velocidade = 10;
     }
 
     public Player(Vector2 position, Texture2D texture) : this()
@@ -46,23 +47,30 @@ public class Player : GameObject
 
         Atualizacao();
 
+        var velocidade = CanMove ? Velocidade : 0;
+
         //KEYBOARD
         if (Keyboard.GetState().IsKeyDown(Keys.Up))
         {
-            Transform.Position.Y -= Velocidade;
+            Transform.Position.Y -= velocidade;
         }
         if (Keyboard.GetState().IsKeyDown(Keys.Down))
         {
-            Transform.Position.Y += Velocidade;
+            Transform.Position.Y += velocidade;
         }
         if (Keyboard.GetState().IsKeyDown(Keys.Left))
         {
-            Transform.Position.X -= Velocidade;
+            Transform.Position.X -= velocidade;
         }
         if (Keyboard.GetState().IsKeyDown(Keys.Right))
         {
-            Transform.Position.X += Velocidade;
+            Transform.Position.X += velocidade;
         }
+
+        if (Keyboard.GetState().IsKeyDown(Keys.Up) || Keyboard.GetState().IsKeyDown(Keys.Down) || Keyboard.GetState().IsKeyDown(Keys.Left) || Keyboard.GetState().IsKeyDown(Keys.Right))
+            CanMove = false;
+        else if (Keyboard.GetState().IsKeyUp(Keys.Up) || Keyboard.GetState().IsKeyUp(Keys.Down) || Keyboard.GetState().IsKeyUp(Keys.Left) || Keyboard.GetState().IsKeyUp(Keys.Right))
+            CanMove = true;
 
         //MOUSE
         if (Mouse.GetState().LeftButton == ButtonState.Pressed)
